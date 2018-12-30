1b5c41c2c9eaf8f8100a9b073defabf6b87e9ca0
Geoff Shannon <geoffpshannon@gmail.com>
2018-12-30 08:10:36 -0800

    Fill out structure of interpreting a Program

--- postfix/src/program.rs
+++ postfix/src/program.rs
@@ -1,6 +1,41 @@
-use crate::parse::Command;
+use std::iter::FromIterator;
+
+use crate::parse::{BuiltIn, Command};
 use crate::top_level::Error as TopLevelError;
 
+#[derive(Clone, Debug, PartialEq)]
+pub enum Error {}
+
+#[derive(Clone, Debug, PartialEq)]
+enum StackValue {
+    ExecutableSequence(Vec<Command>),
+    Integer(i128),
+}
+
+type Stack = Vec<StackValue>;
+
+impl FromIterator<Command> for StackValue {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = Command>,
+    {
+        StackValue::ExecutableSequence(iter.into_iter().collect())
+    }
+}
+
+impl From<Vec<Command>> for StackValue {
+    fn from(commands: Vec<Command>) -> StackValue {
+        StackValue::ExecutableSequence(commands)
+    }
+}
+
+impl From<i128> for StackValue {
+    fn from(number: i128) -> StackValue {
+        StackValue::Integer(number)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct Program {
     num_args: usize,
     commands: Vec<Command>,
@@ -19,7 +54,25 @@ impl Program {
                 actual: num_args,
             });
         }
-
+        let stack: Stack = args.into_iter().rev().map(StackValue::from).collect();
         Err(TopLevelError::Unknown)
+    }
+
+    fn apply_command(mut stack: Stack, command: Command) -> Result<Stack, Error> {
+        match command {
+            Command::Integer(inner) => {
+                stack.push(StackValue::from(inner));
+                Ok(stack)
+            }
+            Command::ExecutableSequence(inner) => {
+                stack.push(StackValue::from(inner));
+                Ok(stack)
+            }
+            Command::BuiltIn(builtin) => Program::apply_builtin(stack, builtin),
+        }
+    }
+
+    fn apply_builtin(mut stack: Stack, builtin: BuiltIn) -> Result<Stack, Error> {
+        Ok(stack)
     }
 }

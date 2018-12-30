56efcc7d8f1793b79ddb0dfa7402cb2bc79a78da
Geoff Shannon <geoffpshannon@gmail.com>
2018-12-30 09:39:09 -0800

    Implement all boolean operators

--- postfix/src/macros.rs
+++ postfix/src/macros.rs
@@ -16,3 +16,22 @@ macro_rules! arith_op {
         Ok($stack)
     }};
 }
+
+#[macro_export]
+macro_rules! bool_op {
+    { $stack:ident, $op:tt } => {{
+        let v1 = $stack
+            .pop()
+            .ok_or(Error::NotEnoughValues)?
+            .into_integer()
+            .ok_or(Error::NotANumber)?;
+        let v2 = $stack
+            .pop()
+            .ok_or(Error::NotEnoughValues)?
+            .into_integer()
+            .ok_or(Error::NotANumber)?;
+
+        $stack.push(StackValue::Integer(if v2 $op v1 {1} else {0}));
+        Ok($stack)
+    }};
+}

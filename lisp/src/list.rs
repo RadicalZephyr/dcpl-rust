62ec5b5d6264d2f4bbe1d032e02c11e9f1e76c8e
Geoff Shannon <geoffpshannon@gmail.com>
2019-01-05 16:37:31 -0800

    Implement first, rest and nth on Lists

--- lisp/src/list.rs
+++ lisp/src/list.rs
@@ -19,3 +19,33 @@ impl FromIterator<Value> for List {
         cell
     }
 }
+
+impl List {
+    pub fn first(&self) -> Option<&Value> {
+        match self {
+            List::Nil => None,
+            List::Cell { ref first, .. } => Some(first),
+        }
+    }
+
+    pub fn rest(&self) -> Option<&Value> {
+        match self {
+            List::Nil => None,
+            List::Cell { ref rest, .. } => Some(rest),
+        }
+    }
+
+    pub fn second(&self) -> Option<&Value> {
+        self.nth(1)
+    }
+
+    pub fn nth(&self, mut idx: usize) -> Option<&Value> {
+        let mut cell = self.rest();
+        idx -= 1;
+        while idx > 0 {
+            cell = cell.and_then(|c| c.as_list()).and_then(|c| c.rest());
+            idx -= 1;
+        }
+        cell.and_then(|c| c.as_list()).and_then(|c| c.first())
+    }
+}
